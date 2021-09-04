0c594bddfff9d4f5635acb8bb0bb4c129e74eab8
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-09-03 21:15:48 -0400

    Add handlers for API and add functionality to form & refresh

---
+++ src/DOMmanipulation.js
@@ -0,0 +1,52 @@
+import { addGameScore, getGameScores } from '.';
+import myScoreBoard from './scoreBoard';
+
+const displayScores = () => {
+  const scoreTable = document.getElementById('scoresTable');
+  const scoreList = scoreTable.firstChild;
+  scoreList.innerHTML = '';
+  const allScores = myScoreBoard.records;
+  allScores.forEach((record) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const scoreCell = document.createElement('td');
+    nameCell.innerHTML = `<h4>${record.user}</h4>`;
+    scoreCell.innerHTML = `<h4>${record.score}</h4>`
+    row.appendChild(nameCell);
+    row.appendChild(scoreCell);
+    scoreList.appendChild(row);
+  });
+}
+
+const addFormSubmission = () => {
+  const nameVal = document.getElementById('name').value;
+  const scoreVal = document.getElementById('score').value;
+  if(nameVal && scoreVal) {
+    addGameScore(nameVal, scoreVal)
+    .then(() => getGameScores())
+    .then(() => displayScores());
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  getGameScores()
+  .then(() => {
+    displayScores();
+  });
+});
+
+const addScoreForm = document.getElementById('score-form');
+addScoreForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addFormSubmission();
+});
+
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', () => {
+  getGameScores()
+  .then(() => {
+    displayScores();
+  });
+});
+
+export { displayScores, addFormSubmission };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import scoresBoard from './gameAPI';
+import myScoreBoard from './scoreBoard';
+import { addFormSubmission } from './DOMmanipulation';
 import './style.css';
 
 const gameURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DcQXhgrrBfecZM5hD6Av/scores/';
+
+export const getGameScores = async () => {
+    try {
+      await fetch(gameURL, {
+        mode: 'cors',
+        method: 'GET'
+      })
+      .then((response) => response.json())
+      .then((data) => myScoreBoard.assignScores(data.result));
+    } catch(error) {
+      myScoreBoard.assignScores(null);
+    }
+};
+
+export const addGameScore = async (name, points) => {
+  try {
+    await fetch(gameURL, {
+      mode: 'cors',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({"user": name, "score": points }),
+    }).then((response) => {
+      console.log(response.json());
+    });
+  } catch(error) {
+    console.log(error);
+  }
+};
